0e281b5f9c63eeef2e499c9185b06c34a5007692
unknown <gaetano@daidone.dev>
2019-09-09 21:14:49 -0400

    :label: Navbar
    added navbar #1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar'
 import './App.css';
 
 class App extends Component {
   render() {
+    const name = 'John Doe';
+
     return (
       <div className='App'>
-        <h1>Hello</h1>
+        <Navbar title="Github Finder" icon='fab fa-github' />
       </div>
     );
   }
